f867c961a88188fce21a226bd1846665be6049c8
Mario <neo_marcos29@yahoo.com>
2019-05-06 02:33:30 -0700

    -Added data skeleton for ChooseRestaurantFrags

--- app/src/main/java/com/example/findmefood/ChooseRestaurantFragment.java
+++ app/src/main/java/com/example/findmefood/ChooseRestaurantFragment.java
@@ -17,6 +17,8 @@ import com.example.findmefood.models.Coordinates;
 import com.example.findmefood.models.Restaurant;
 import com.squareup.picasso.Picasso;
 
+import java.util.List;
+
 
 public class ChooseRestaurantFragment extends Fragment{
     Button mActionNavigate, mActionInformation;
@@ -24,6 +26,7 @@ public class ChooseRestaurantFragment extends Fragment{
     private static final String TAG = ChooseRestaurantFragment.class.getName();
     public static final String RESTAURANT = "RESTAURANT";
     private static final String URL = "URL";
+    private static final int ONE_MILE = 1609;
     public enum Mode {
         DRIVE("d"),BIKE("b"),MOTOR("l"),WALK("w");
         private String mode;
@@ -52,25 +55,37 @@ public class ChooseRestaurantFragment extends Fragment{
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 //        mActivity = getActivity();
         final Restaurant restaurant = (Restaurant) getArguments().getSerializable(RESTAURANT);
-        final String name =  restaurant.getName();
-        final String url = restaurant.getUrl();
+        final String name =  restaurant.getName(), url = restaurant.getUrl(), price = restaurant.getPrice();
         final String image_url = restaurant.getImage_url();
         final Coordinates coordinates = restaurant.getCoordinates();
+        final List<String> address_list = restaurant.getLocation().showAddress();
+        final Double rating = restaurant.getRating(), distance = restaurant.getDistance()/ONE_MILE;
+        String address = "";
+        StringBuilder sb = new StringBuilder();
+        for(String address_item: address_list){
+            sb.append(address_item + " ");
+        }
+        address = sb.toString();
+
+        Log.d(TAG, "Address : " + address);
+
         View v = inflater.inflate(R.layout.fragment_ff_chooserestaurant,container,false);
-        TextView mTextView = (TextView)v.findViewById(R.id.cr_textview);
+        TextView restaurantNameTextView = (TextView)v.findViewById(R.id.cr_textview), restaurantPriceTextView = (TextView)v.findViewById(R.id.cr_text_price),
+                distanceTextView = (TextView)v.findViewById(R.id.cr_text_distance), addressTextView = (TextView)v.findViewById(R.id.cr_text_address),
+                ratingTextView = (TextView)v.findViewById(R.id.cr_text_rating);
         imageView = (ImageView)v.findViewById(R.id.cr_imageview);
         mActionNavigate = v.findViewById(R.id.cr_action_navigate);
         mActionInformation = v.findViewById(R.id.cr_action_information);
         Log.d(TAG,"Restaurant name: " + name);
         Log.d(TAG,"Coordinates" + coordinates.toString());
-        mTextView.setText(name);
-        Log.d(TAG,"Image url :" + image_url);
+        restaurantNameTextView.setText(name);
         if(image_url != null && !(image_url.isEmpty())){
             Picasso.get().load(image_url).error(R.mipmap.ic_launcher).placeholder(R.drawable.cr_placeholder).fit().centerCrop().into(imageView);
         }
-
-        //Todo: Add more information in screen
-
+        restaurantPriceTextView.setText(price);
+        ratingTextView.setText(String.format("%.2f stars",rating));
+        addressTextView.setText(address);
+        distanceTextView.setText(String.format("%.2f mi",distance));
 
         mActionNavigate.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -92,11 +107,6 @@ public class ChooseRestaurantFragment extends Fragment{
         mActionInformation.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //Launch Information Activity
-                //TODO
-//                Intent yelpSiteIntent = new Intent(Intent.ACTION_VIEW);
-//                yelpSiteIntent.setData(Uri.parse(url));
-//                startActivity(yelpSiteIntent);
                 Intent yelpSiteIntent = new Intent(getActivity().getBaseContext(), WebViewActivity.class);
                 yelpSiteIntent.putExtra(URL,url);
                 startActivity(yelpSiteIntent);
